e1746492476ca1953935c61f96d56abee0f528e2
Sakthivel C <sakthislm@gmail.com>
2015-05-10 03:11:14 +0530

    Added icons for cabs & minor UI changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,21 @@
+var sourceLatLng, destLatLng;
+
 new gnMenu( document.getElementById( 'gn-menu' ) );
+
+var vehicleIconEnum = {
+  'UberX': 'icons/uber-small-32px.png',
+  'UberTaxi': 'icons/uber-medium-32px.png',
+  'UberBlack': 'icons/uber-large-32px.png',
+  'OlaX': 'icons/ola-small-32px.png',
+  'OlaTaxi': 'icons/ola-medium-32px.png',
+  'OlaBlack': 'icons/ola-large-32px.png',
+  'T4SX': 'icons/t4s-small-32px.png',
+  'T4STaxi': 'icons/t4s-medium-32px.png',
+  'T4SBlack': 'icons/t4s-large-32px.png',
+  'MeruX': 'icons/meru-small-32px.png',
+  'MeruTaxi': 'icons/meru-medium-32px.png',
+  'MeruBlack': 'icons/meru-large-32px.png'
+}
 
 function handleNoGeolocation(errorFlag) {
   if (errorFlag) {
@@ -24,7 +41,24 @@ function handleNoGeolocation(errorFlag) {
   google.maps.event.addListener(autocomplete, 'place_changed', function() {
     console.log("Place changed in Search box!");
   });
+
+  destinationAutocomplete = new google.maps.places.Autocomplete(
+      (document.getElementById('destination-selection')),
+      { types: ['geocode'] });
+  google.maps.event.addListener(destinationAutocomplete, 'place_changed', function() {
+    console.log("Place changed in Destination box!");
+  });
 }());
+
+function plotCar(map, lat, lng, carType) {
+  var geolocation = new google.maps.LatLng(lat, lng);
+
+  map.addMarker({
+    pos: geolocation,
+    draggable: false,
+    icon: vehicleIconEnum[carType],
+  });
+}
 
 (function(window, mapster) {
 
@@ -51,7 +85,7 @@ function handleNoGeolocation(errorFlag) {
         pos: geolocation,
         icon: {
           path: google.maps.SymbolPath.CIRCLE,
-          strokeColor: 'DodgerBlue',
+          strokeColor: '#eb5d1e',
           strokeWeight: 5,
           scale: 8
         }
@@ -61,7 +95,7 @@ function handleNoGeolocation(errorFlag) {
         pos: geolocation,
         draggable: true,
         showContent: true,
-        content: '<h5>Pickup location</h5>',
+        content: '<h5>Choose a pickup location</h5>',
         icon: 'icons/pin-64px.png',
         event: {
           name: 'dragend',
@@ -72,6 +106,8 @@ function handleNoGeolocation(errorFlag) {
         }
       });
       pickupLocationMarker.setZIndex(1000);
+
+      plotCar(map, position.coords.latitude+0.001, position.coords.longitude-0.003, 'UberX');
     }, function() {
       handleNoGeolocation(true);
     });
